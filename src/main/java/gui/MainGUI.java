d78a6b7642c4dd604884ccf4b5f403d9e6403137
Dara <72565678+Dara-PP@users.noreply.github.com>
2022-04-07 20:51:55 +0200

    Update MainGUI.java

--- src/main/java/gui/MainGUI.java
+++ src/main/java/gui/MainGUI.java
@@ -1,13 +1,12 @@
 package gui;
 
-import java.awt.BorderLayout;
-import java.awt.Container;
-import java.awt.Dimension;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-import javax.swing.JFrame;
-import javax.swing.JTextField;
+import javax.swing.*;
 
 import config.GameConfiguration;
 import engine.map.Block;
